803017b081a009670ce9df9002a3fd3d2631074b
Nitin <nitinjha2609@gmail.com>
2024-07-04 19:19:18 +0530

    done

---
+++ script.js
@@ -0,0 +1,9 @@
+async function getData(){
+    let data=await axios.get("https://official-joke-api.appspot.com/random_joke");
+    console.log(data);
+    document.getElementById("joke").innerText=data.data.setup;
+    document.getElementById("punch").innerText=data.data.punchline;    
+}
+
+getData();
+document.getElementById("reload").addEventListener("click", getData)
